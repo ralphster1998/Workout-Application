a4ecd8544142a98545dd8778b71bf19aa9a1b907
Ralph <ralphstevendc@gmail.com>
2020-07-14 16:04:16 -0700

    template for the redux

--- WorkoutApp/src/components/App.js
+++ WorkoutApp/src/components/App.js
@@ -10,16 +10,10 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../reducers/ExerciseReducer';
+import reducers from '../reducers/PeopleReducer';
+import PeopleList from './PeopleList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -27,7 +21,7 @@ export default class App extends Component<Props> {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to the workout app!!</Text>
+          <PeopleList />
         </View>
       </Provider>
     );
@@ -40,15 +34,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
