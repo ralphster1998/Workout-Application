139d0b5a812260e5c7f79f8f9f3cb3ca408ae4cc
Ralph <ralphstevendc@gmail.com>
2020-07-18 15:04:59 -0700

    added some more items to the detail view

--- WorkoutApp/src/components/ExerciseDetail.js
+++ WorkoutApp/src/components/ExerciseDetail.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Text, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, Text, Image, TouchableOpacity} from 'react-native';
 import { connect } from 'react-redux';
 import { AntDesign } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons'; 
 import * as actions from '../actions';
 
 const styles = StyleSheet.create({
@@ -27,11 +29,49 @@ class ExerciseDetail extends Component {
                         // add stylle if needed
                     />
                     <AntDesign name={'user'} size={80} style={styles.icon} />
-                    <AntDesign name={'close'} size={30} style={styles.close} 
+                    <AntDesign name={'close'} size={30} style={styles.closeIcon} 
                         onPress={() => this.props.noneSelected()}
                     />
                     <Text style={styles.title1}>{this.props.exercise.exerciseName}</Text>
                     <Text style={styles.title2}>{this.props.exercise.category}</Text>
+
+                    <View style={styles.textArea}>
+                        <MaterialIcons name="confirmation-number" size={30} style={styles.textIcons} />                        
+                        <Text style={}>{this.props.exercise.currentReps}</Text>
+                    </View>
+                    <View style={styles.textArea}>
+                        <Entypo name="reply" size={30} style={styles.textIcons} />                        
+                        <Text style={}>{this.props.exercise.goalReps}</Text>
+                    </View>
+                    <View style={styles.textArea}>
+                        <Entypo name="reply-all" size={30} style={styles.textIcons} />                       
+                        <Text style={}>{this.props.exercise.url}</Text>
+                    </View>
+                    <View>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/call@2x.png.png')}
+                                style={styles.actionImage}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/email@2x.png.png')}
+                                style={styles.actionImage}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image 
+                                source={require('../images/sms@2x.png.png')}
+                                style={styles.actionImage}
+                            />
+                        </TouchableOpacity>
+                        <View style={styles.actionArea}>
+                            <Text>Current</Text>
+                            <Text>Goal</Text>
+                            <Text>URL</Text>
+                        </View>
+                    </View>
                 </ScrollView>
             </View>
         )
